4417b9930bb467e1192e3a5bad5503a9acf0fe20
MohamedA-M-Hassan <anehmr0@gmail.com>
2016-05-01 03:31:40 +0200

    insert the hole size and it's address

---
+++ mainwindow.cpp
@@ -0,0 +1,39 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_insertButton_clicked()
+{
+    int x= ui->insert->text().toInt();
+    int y= ui->insert2->text().toInt();
+    hole.push_back(qMakePair(x,y));
+}
+
+
+void MainWindow::on_bestFit_clicked()
+{
+
+}
+
+void MainWindow::on_firstFit_clicked()
+{
+
+}
+
+void MainWindow::on_processInsertButton_clicked()
+{
+    int x = ui->insert_2->text().toInt();
+    processSize.push_back(x);
+}
---
+++ mainwindow.h
@@ -0,0 +1,36 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+
+    void on_insertButton_clicked();
+
+    void on_radioButton_clicked();
+
+    void on_bestFit_clicked();
+
+    void on_firstFit_clicked();
+
+    void on_processInsertButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QVector <int> processSize;
+    QVector <QPair < double,double > > hole;
+};
+
+#endif // MAINWINDOW_H
